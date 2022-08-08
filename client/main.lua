3848adc7284de1c00155c1f17061b8ab9f66e801
Pedro Rodrigues <110211861+redyydev@users.noreply.github.com>
2022-08-08 23:39:11 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -2,9 +2,7 @@ local Framework = nil
 
 if Config.Framework == "qb-core" then
     Framework = exports['qb-core']:GetCoreObject()
-
 elseif Config.Framework == "esx" then
-    
     Framework = exports['es_extended']:getSharedObject()
 end
 
